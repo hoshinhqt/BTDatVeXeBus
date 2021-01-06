c06f9e190da57202b2810d767e451ea9f20781cf
hoshinhqt <31725839+hoshinhqt@users.noreply.github.com>
2021-01-06 18:02:51 +0700

    Input , output, Viewchild

---
+++ btVeDatXeBus/src/app/_core/Modules/Ghe.ts
@@ -0,0 +1,9 @@
+export class Ghe { 
+
+        SoGhe :number =  1;
+        TenGhe : String =  'số 1';
+        Gia : number =  100;
+        TrangThai : boolean = false ;
+       constructor(){
+       }
+}
